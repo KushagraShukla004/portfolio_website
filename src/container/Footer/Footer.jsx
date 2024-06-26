04db1c45de849f262efa5ff53083e5b540a3cfac
Kushagra_Shukla <73269482+KushagraShukla004@users.noreply.github.com>
2024-06-26 16:11:41 +0530

    Updated Responsiveness of Containers

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,14 +1,14 @@
-import React, { useState } from 'react';
-import { images } from '../../constants';
-import { AppWrap, MotionWrap } from '../../wrapper';
-import { client } from '../../client';
-import { BiUpArrowCircle } from 'react-icons/bi';
-import './Footer.scss';
+import React, { useState } from "react";
+import { images } from "../../constants";
+import { AppWrap, MotionWrap } from "../../wrapper";
+import { client } from "../../client";
+import { BiUpArrowCircle } from "react-icons/bi";
+import "./Footer.scss";
 
 const scrollToTop = () => {
   window.scrollTo({
     top: 0,
-    behavior: 'smooth',
+    behavior: "smooth",
     /* you can also use 'auto' behaviour
          in place of 'smooth' */
   });
@@ -16,9 +16,9 @@ const scrollToTop = () => {
 
 const Footer = () => {
   const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    message: '',
+    name: "",
+    email: "",
+    message: "",
   });
   const [isFormSubmitted, setIsFormSubmitted] = useState(false);
   const [loading, setLoading] = useState(false);
@@ -34,7 +34,7 @@ const Footer = () => {
     setLoading(true);
 
     const contact = {
-      _type: 'contact',
+      _type: "contact",
       name: formData.username,
       email: formData.email,
       message: formData.message,
@@ -50,58 +50,58 @@ const Footer = () => {
   };
   return (
     <>
-      <h2 className='head-text'>
+      <h2 className="head-text">
         Contact <span>Me</span>
       </h2>
-      <div className='app__footer-cards'>
-        <div className='app__footer-card'>
-          <img src={images.email1} alt='email' />
-          <a href='mailto:skushagra0105@gmail.com' className='mail-p-text'>
-            skushagra0105@gmail.com
+      <div className="app__footer-cards">
+        <div className="app__footer-card">
+          <img src={images.email1} alt="email" />
+          <a href="mailto:skushagra0105@gmail.com" className="mail-p-text">
+            <p>skushagra0105@gmail.com</p>
           </a>
         </div>
       </div>
       {!isFormSubmitted ? (
-        <div className='app__footer-form app__flex'>
-          <div className='app__flex'>
+        <div className="app__footer-form app__flex">
+          <div className="app__flex">
             <input
-              className='p-text'
-              type='text'
-              placeholder='Your Name'
-              name='username'
+              className="p-text"
+              type="text"
+              placeholder="Your Name"
+              name="username"
               value={username}
               onChange={handleChangeInput}
             />
           </div>
-          <div className='app__flex'>
+          <div className="app__flex">
             <input
-              className='p-text'
-              type='email'
-              placeholder='Your Email'
-              name='email'
+              className="p-text"
+              type="email"
+              placeholder="Your Email"
+              name="email"
               value={email}
               onChange={handleChangeInput}
             />
           </div>
           <div>
             <textarea
-              className='p-text'
-              placeholder='Your Message'
+              className="p-text"
+              placeholder="Your Message"
               value={message}
-              name='message'
+              name="message"
               onChange={handleChangeInput}
             />
           </div>
-          <button type='button' className='p-text' onClick={handleSubmit}>
-            {!loading ? 'Send Message' : 'Sending...'}
+          <button type="button" className="p-text" onClick={handleSubmit}>
+            {!loading ? "Send Message" : "Sending..."}
           </button>
         </div>
       ) : (
         <div>
-          <h3 className='head-text'>Thank you for getting in touch!</h3>
+          <h3 className="head-text">Thank you for getting in touch!</h3>
         </div>
       )}
-      <button className='scrolltop' onClick={scrollToTop}>
+      <button className="scrolltop" onClick={scrollToTop}>
         <BiUpArrowCircle size={30} />
       </button>
     </>
@@ -109,8 +109,8 @@ const Footer = () => {
 };
 
 export default AppWrap(
-  MotionWrap(Footer, 'app__footer'),
-  'contact',
+  MotionWrap(Footer, "app__footer"),
+  "contact",
   // 'app__whitebg'
-  'app__Secondary-darkmode'
+  "app__Secondary-darkmode"
 );
