04f8b4c9eff4e9f460cc13c4a183b3b9878cbb48
Kushagra_Shukla <73269482+KushagraShukla004@users.noreply.github.com>
2024-07-08 16:27:14 +0530

    Resume Download Event Analytics

--- src/constants/ResumeButton.js
+++ src/constants/ResumeButton.js
@@ -1,9 +1,27 @@
 import React from "react";
 import { images } from "../constants";
 import "./Resume.scss";
+
 const ResumeButton = () => {
+  const handleResumeDownload = () => {
+    window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push({
+      event: "resume_download",
+      category: "Resume",
+      action: "Download",
+      label: "Resume PDF",
+    });
+  };
+
   return (
-    <a href={images.resume} alt="Download Resume">
+    <a
+      id="resume-download"
+      href={images.resume}
+      alt="Download Resume"
+      onClick={handleResumeDownload}
+      target="_blank"
+      rel="noopener noreferrer"
+    >
       <button>
         <span style={{ fontSize: "25px" }}>Resume</span>
       </button>
